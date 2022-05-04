2c0f29a9e9643393a3cdb3a80dae48a2c97ce5a4
Emmerick Fontaine <Emmerick.Fontaine@gmail.com>
2022-05-04 14:52:04 +0200

    base

---
+++ main.py
@@ -0,0 +1,40 @@
+from ast import Str
+from flask import Blueprint, redirect, render_template, Flask,request,flash
+from flask_login import login_required, current_user
+from . import db
+from .models import Ticket, User, Reponse
+import time
+main = Blueprint('main', __name__)
+#page index.html
+@main.route('/',)
+def index():
+    db.create_all()
+    return render_template("index.html")
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+@main.route('/ticket',methods = ['GET', 'POST'])
+@login_required
+def ticket():
+    if request.method == 'POST':
+        if not request.form['sujet'] or not request.form['objet']:
+            flash('Veuillez renseigner tous les champs (sujet et objet)', 'error')
+        else:
+            ticket=Ticket(request.form["sujet"],request.form["objet"],request.form["user_id"],"null","1"    )
+            db.session.add(ticket)
+            db.session.commit()
+    return render_template('ticket.html', name=current_user.name,id = current_user.id,tickets = Ticket.query.filter_by(user_id=current_user.id).order_by(Ticket.id.desc()))
+@main.route('/voirticket',methods = ['GET', 'POST'])
+@login_required
+def voirticket():
+    id_ticket = request.args.get('arg1')
+    if request.method == 'POST':
+        if not request.form['reponse']:
+            flash('Veuillez renseigner tous les champs (sujet et objet)', 'error')
+        else:
+            reponse=Reponse(request.form["reponse"],request.form["id"])
+            db.session.add(reponse)
+            db.session.commit()
+            return redirect('/voirticket?arg1='+str(request.form["id"]))
+    return render_template('voirticket.html', name=current_user.name,id = current_user.id,tickets = Ticket.query.filter_by(user_id=current_user.id).filter_by(id=id_ticket),id_ticket=id_ticket,reponses = Reponse.query.filter_by(id_reponse=id_ticket))    
---
+++ models.py
@@ -0,0 +1,35 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    admin= db.Column(db.String(1))
+    log = db.relationship('Ticket', backref='user')
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sujet = db.Column(db.String(30), nullable=False)
+    objet = db.Column(db.String(1000), nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    admin_id=db.Column(db.Integer)
+    etat=db.Column(db.String(20),nullable=False)
+    id_reponse = db.relationship('Reponse', backref='ticket')
+
+    def __init__(self, sujet,objet,user_id,admin_id,etat):
+      self.sujet = sujet
+      self.objet = objet
+      self.user_id = user_id
+      self.admin_id = admin_id
+      self.etat=etat
+
+class Reponse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    reponse= db.Column(db.String(1000), nullable=False)
+    id_reponse=db.Column(db.Integer, db.ForeignKey('ticket.id'),nullable=False)
+
+    def __init__(self,reponse,id_reponse):
+        self.reponse = reponse
+        self.id_reponse = id_reponse
